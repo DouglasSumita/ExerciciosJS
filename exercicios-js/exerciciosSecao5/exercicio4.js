4fcc004c43e84c96809acf17007469ce086dd0d2
Douglas Sumita <douglas.sumita@gmail.com>
2020-07-30 21:21:19 -0300

    Exercicio 4 Curso JavaScript

---
+++ exercicios-js/exerciciosSecao5/exercicio4.js
@@ -0,0 +1,12 @@
+/* 
+04) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+e o resto da divisão destes dois valores.
+
+*/
+
+function divisaoResto(x, y) {
+    console.log('Divisão =', x / y)
+    console.log('Resto =', x % y)
+}
+
+divisaoResto(10, 3)
