cb00c33c471ef6e7d35b541835b67fe52383d7ab
Douglas Sumita <douglas.sumita@gmail.com>
2020-08-05 19:33:10 -0300

    Correcao para utilizar a funcao exportada

--- exercicios-js/exerciciosSecao5/exercicio5.js
+++ exercicios-js/exerciciosSecao5/exercicio5.js
@@ -6,7 +6,8 @@ um exercício simples para mostrar dinheiro sempre da forma correta. Desenvolva 
 que ela receba um valor como 0.30000000000000004 e retorne R$0,30 (observe a vírgula e o ponto).
 */
 
-const converteParaBRL = (valor) => 'R$' + valor.toFixed(2).replace('.',',') //Arrow function
+const converteParaBRL = (valor) => 'R$ ' + valor.toFixed(2).replace('.',',') //Arrow function
 
-console.log(converteParaBRL(0.2 + 0.1))
-
+module.exports = {
+    converteParaBRL
+}
