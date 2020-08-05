ee9cbefdb52e68d245767b428a8bab5975029701
Douglas Sumita <douglas.sumita@gmail.com>
2020-08-05 18:08:37 -0300

    Estrutura condicional switch que recebe uma string com o nome de uma fruta e possua tres casos: maçã, kiwi ou melancia. e retorna um

---
+++ exercicios-js/exerciciosSecao5/pedidoFrutaria.js
@@ -0,0 +1,32 @@
+/*
+Crie uma estrutura condicional switch que receba uma string com o nome de uma fruta e que possua três
+casos: Caso maçã, retorne no console: “Não vendemos esta fruta aqui”. Caso kiwi, retorne: “Estamos com
+escassez de kiwis”. Caso melancia, retorne: “Aqui está, são 3 reais o quilo”. Teste com estas três opções .Crie
+também um default, que retornará uma mensagem de erro no console.
+*/
+
+function pedidoFrutaria(fruta) {
+    
+    let mensagem = ''
+
+    switch (fruta) {
+        case 'maçã':
+            mensagem = 'Não vendemos esta fruta aqui'
+            break
+        case 'kiwi':
+            mensagem = 'Estamos com escassez de kiwis'
+            break
+        case 'melancia':
+            mensagem = 'Aqui está, são 3 reais o quilo'
+            break
+        default:
+            mensagem = 'Erro no console'    
+    }
+
+    return console.log(mensagem)
+}
+
+pedidoFrutaria('maçã')
+pedidoFrutaria('kiwi')
+pedidoFrutaria('melancia')
+pedidoFrutaria()
